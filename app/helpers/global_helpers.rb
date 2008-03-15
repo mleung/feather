b594482a5e438cea043853265f3a82bd65650554
Michael Leung <blamemike@gmail.com>
2008-03-14 20:16:33 -0400

    Put in relative time for posted_at

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -2,9 +2,23 @@ module Merb
   module GlobalHelpers
     
     def textile_to_html(content)
-      r = RedCloth.new(content)
-      r.to_html
+      RedCloth.new(content).to_html
     end
-    
+
+    def render_relative_date(date)
+      date = Date.parse(date, true) unless /Date.*/ =~ date.class.to_s
+      days = (date - Date.today).to_i
+
+      return 'today'     if days >= 0 and days < 1
+      return 'tomorrow'  if days >= 1 and days < 2
+      return 'yesterday' if days >= -1 and days < 0
+
+      return "in #{days} days"      if days.abs < 60 and days > 0
+      return "#{days.abs} days ago" if days.abs < 60 and days < 0
+
+      return date.strftime('%A, %B %e') if days.abs < 182
+      return date.strftime('%A, %B %e, %Y')
+    end
+
   end
 end

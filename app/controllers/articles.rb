9d8117a765b25aecdb3ebef22ece8909caed878c
El Draper <el@eldiablo.co.uk>
2008-04-12 17:49:46 +0100

    replaced separate permalink rack handler with a more clever deferred merb route

--- app/controllers/articles.rb
+++ app/controllers/articles.rb
@@ -1,7 +1,6 @@
 class Articles < Application
   # This handles the index (recent articles), or the year/month/day views
   def index
-    # Unfortunately, we can't use merb-action-args here because we're using the custom rack handler. Booo.
     @archives = Article.get_archive_hash
     if params[:day]
       @articles = Article.find_by_year_month_day(params[:year], params[:month], params[:day])
@@ -15,14 +14,8 @@ class Articles < Application
     display @articles
   end
   
-  # This handles the permalink for articles, and is executed using the special permalink Rack handler
-  def show
-    if @article = Article.find_by_permalink(request.uri.to_s.chomp("/"))
-      # This will render the article and the request will not process any further
-      display @article
-    else
-      # This will force Rack to pass the request off to the main Merb app, as we didn't find a post for the url
-      self.status = 404
-    end
+  def show(id)
+    @article = Article[id]
+    display @article
   end
 end
--- config/rack.rb
+++ config/rack.rb
@@ -1,19 +1,5 @@
-# This is designed to pick up requests to any configured permalink, allowing existing links to be retained
-# and new permalinks to be intercepted and processed
-# Probably a bit too complicated for a route, hence a dedicated handler
-permalink = Proc.new do |env|
-  request = Merb::Request.new(env)
-  controller = Articles.new(request, 200)
-  if Merb::Dispatcher.use_mutex
-    Merb::Dispatcher.class_eval("@@mutex").synchronize { controller._dispatch(:show) }
-  else
-    controller._dispatch(:show)
-  end
-  [controller.status, controller.headers, controller.body]
-end
-
 # Create the Merb application
 merb = Merb::Rack::Application.new
 
-# Tell Rack to run the permalink handler first, and if no article can be found for the request, then pass it to Merb
-run Rack::Cascade.new([permalink, merb])
+# Tell Rack to run the Merb application
+run Rack::Cascade.new([merb])

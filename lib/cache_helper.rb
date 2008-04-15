82b2bad8449d8a3100448fbdb9c5e4258355c12c
Michael Leung <blamemike@gmail.com>
2008-04-15 01:59:09 -0400

    Fixing an issue where an unpublished article causes an error in the caching code, as it has no permalink yet.

--- lib/cache_helper.rb
+++ lib/cache_helper.rb
@@ -4,12 +4,16 @@ module CacheHelper
   end
   
   def expire_article(article)
-    parts = article.permalink.split("/")
-    # Expire the year. 
-    expire_page(:controller => "/", :action => parts[1])
-    # Expire the months
-    expire_page(:controller => "/", :action => "#{parts[1]}/#{parts[2]}")
-    expire_page(:controller => '/', :action => article.permalink)
+    # If an article is a draft, it
+    # will not have a permalink to split on.
+    unless article.permalink.nil?
+      parts = article.permalink.split("/")
+      # Expire the year. 
+      expire_page(:controller => "/", :action => parts[1])
+      # Expire the months
+      expire_page(:controller => "/", :action => "#{parts[1]}/#{parts[2]}")
+      expire_page(:controller => '/', :action => article.permalink)
+    end
   end
   
 end

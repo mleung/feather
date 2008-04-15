cde24ffc745ecfc07538cede3fabb7c3ba878f76
Michael Leung <blamemike@gmail.com>
2008-04-15 15:31:58 -0400

    Put in nil check before gsubbing the about text

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -15,9 +15,11 @@ class Configuration < DataMapper::Base
   # This returns a shortened about for displaying on the settings screen, if the about is multiple lines
   def about_summary
     summary = self.about
-    summary = summary.gsub("\r\n", "\n")
-    summary = "#{summary[0..summary.index("\n") - 1]}..." if summary.index("\n")
-    summary
+    unless summary.nil? || summary.empty?
+      summary = summary.gsub("\r\n", "\n")
+      summary = "#{summary[0..summary.index("\n") - 1]}..." if summary.index("\n")
+      summary
+    end
   end
   
   ##

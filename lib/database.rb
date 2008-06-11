32187e0bc4d0d45ed19e32c8d06069561f4f8aa3
El Draper <el@eldiablo.co.uk>
2008-06-11 21:38:29 +0100

    added a db task to auto migrate all core classes for feather (for initial database setup)

--- lib/database.rb
+++ lib/database.rb
@@ -1,6 +1,6 @@
 module Database
   class << self
-    CORE = [Activity, Article, Configuration, Plugin, User]
+    CORE = Dir.glob("app/models/*.rb").collect { |s| Class.const_get(s.split("/").last.gsub(".rb", "").split("_").collect { |w| w.capitalize }.join("")) }
     
     # This provides a helper method for data migration for plugins - we can then update this to use non-destructive migrations at a later date and existing plugins won't need to change
     def migrate(klass)
@@ -10,6 +10,11 @@ module Database
       raise "Class cannot be migrated!" unless klass.respond_to?(:auto_migrate!)
       # Execute auto migrations for now
       klass.auto_migrate!
+    end
+    
+    # This does the initial auto migration of all core classes
+    def initial_setup
+      CORE.each { |c| c.auto_migrate! }
     end
   end
 end

18ebc879e692e23b947c3ab5688d7b2a9d45eae6
El Draper <el@eldiablo.co.uk>
2008-06-11 22:38:24 +0100

    fixing the Merb 0.9.3 cache issue at long last within Feather, as trying to get people to use only Merb 0.9.2 is becoming difficult - this can be removed once Merb 0.9.4 is released (the cache issue is fixed within edge)

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -5,6 +5,13 @@ class Application < Merb::Controller
   before :get_settings
   before :load_plugins
   before :fire_before_event
+  before :fix_cache_issue_with_merb_093
+  
+  ##
+  # This just makes sure that params[:format] isn't null, to get around the merb 0.9.3 cache issue
+  def fix_cache_issue_with_merb_093
+    params[:format] = [] if params[:format].nil?
+  end
 
   ##
   # This grabs settings

9371928048a55624daa011ec461f13b6ee368e66
El Draper <el@eldiablo.co.uk>
2008-11-26 01:06:03 +0000

    updated to use latest merb

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,5 +1,5 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0"
+merb_gems_version = "1.0.3"
 dm_gems_version   = "0.9.7"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components

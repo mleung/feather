f14d3fe5999cad1679195c82dd32d154be571f92
El Draper <el@eldiablo.co.uk>
2008-12-30 01:37:02 +0000

    updating the dependency file to work with the latest Merb/DM

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,6 +1,6 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.3"
-dm_gems_version   = "0.9.7"
+merb_gems_version = "1.0.7"
+dm_gems_version   = "0.9.8"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-action-args", merb_gems_version

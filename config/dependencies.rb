d493979786262113e6086faca99ed9f7555c3227
El Draper <el@eldiablo.co.uk>
2009-03-23 23:28:49 +0000

    updating dependencies in line with latest Merb version

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,5 +1,5 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.9"
+merb_gems_version = "1.0.10"
 dm_gems_version   = "0.9.10"
 do_gems_version   = "0.9.11"
 
